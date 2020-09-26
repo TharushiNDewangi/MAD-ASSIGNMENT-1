dfdf9c04c7731e2e7961610211195effafde5213
TharushiNDewangi <dewsdrv@gmail.com>
2020-09-26 16:24:56 +0530

    course management function 3nd

---
+++ app/src/main/java/com/example/elearningsystem/AddCourseHelper.java
@@ -0,0 +1,57 @@
+package com.example.elearningsystem;
+
+public class AddCourseHelper {
+
+    String coursename;
+    String title;
+    String des;
+    String mimageurl;
+
+    public AddCourseHelper(String coursename, String title, String des, String mimageurl) {
+        this.coursename = coursename;
+        this.title = title;
+        this.des = des;
+        this.mimageurl = mimageurl;
+    }
+
+    public AddCourseHelper(String coursename, String title, String des) {
+        this.coursename = coursename;
+        this.title = title;
+        this.des = des;
+    }
+
+    public AddCourseHelper() {
+    }
+
+    public String getMimageurl() {
+        return mimageurl;
+    }
+
+    public void setMimageurl(String mimageurl) {
+        this.mimageurl = mimageurl;
+    }
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+}
--- app/src/main/java/com/example/elearningsystem/Course.java
+++ app/src/main/java/com/example/elearningsystem/Course.java
@@ -1,20 +1,36 @@
 package com.example.elearningsystem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.renderscript.ScriptGroup;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class Course extends AppCompatActivity {
 
@@ -24,6 +40,11 @@ public class Course extends AppCompatActivity {
     Button b1;
     ImageView i1;
     public Uri imageUrl;
+    FirebaseDatabase rootnode;
+    DatabaseReference ref;
+    private StorageReference refstorage;
+   // private DatabaseReference
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +52,8 @@ public class Course extends AppCompatActivity {
 
         b1=findViewById(R.id.add);
         e1=findViewById(R.id.CourseTitle);
+        e2 =findViewById(R.id.coursedescription);
+        s = (Spinner) findViewById(R.id.spinner);
         i1=findViewById(R.id.imageView);
         i1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,27 +64,92 @@ public class Course extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //addcourse();
+                addcourse();
             }
         });
     }
 
     public void addcourse() {
-        String text= e1.getText().toString();
-        if(text.isEmpty())
-        {
-            alert("Please Insert Title");
-        }
+        rootnode = FirebaseDatabase.getInstance();
+        ref = rootnode.getReference("course");
+        refstorage = FirebaseStorage.getInstance().getReference("course");
 
-        Toast.makeText(getApplicationContext(), "Successfully added", Toast.LENGTH_SHORT).show();
+//
+//        //get all the values
+//        String title = e1.getText().toString();
+//        String des = e2.getText().toString();
+//        String course = s.getSelectedItem().toString();
+
+        //final String img ;
+        if (i1 != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+//            StorageReference fileref = refstorage.child( System.currentTimeMillis() + "." + getFileExtension(imageUrl));
+//            fileref.putFile(imageUrl)
+            StorageReference fileref = refstorage.child( "images/*"+ UUID.randomUUID().toString());
+            fileref.putFile(imageUrl)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                           // Handler handler=new Handler()
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "upload susseed", Toast.LENGTH_SHORT).show();
+
+
+                            //get all the values
+                            String title = e1.getText().toString();
+                            String des = e2.getText().toString();
+                            String course = s.getSelectedItem().toString();
+
+                            AddCourseHelper helper = new  AddCourseHelper(title,des,course,taskSnapshot.toString());
+                            ref.child(title).setValue(helper);
+
+                            Toast.makeText(getApplicationContext(), "Successfully added", Toast.LENGTH_SHORT).show();
+//                            img = taskSnapshot.toString();
+                            //helper
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                            .getTotalByteCount());
+                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "no file selected", Toast.LENGTH_SHORT).show();
+        }
+        //String img = i1.getDownloaed.toString();
+//        AddCourseHelper helper = new  AddCourseHelper(title,des,course);
+//        ref.child(title).setValue(helper);
+//
+//        Toast.makeText(getApplicationContext(), "Successfully added", Toast.LENGTH_SHORT).show();
 
         //call second activity
-        Intent intent = new Intent(this,viewcourse.class);
+        //Intent intent = new Intent(this,viewcourse.class);
         //
 
-        startActivity(intent);
+        //startActivity(intent);
 
     }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver CR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(CR.getType(uri));
+
+    }
+
     private void alert(String message)
     {
         AlertDialog dlg = new AlertDialog.Builder(Course.this).setTitle("Message").setMessage(message)
@@ -86,7 +174,9 @@ public class Course extends AppCompatActivity {
         {
             imageUrl = data.getData();
             i1.setImageURI(imageUrl);
+            //picasso.with(this).load(imageUrl).into(i1);
         }
+
 
     }
 }
